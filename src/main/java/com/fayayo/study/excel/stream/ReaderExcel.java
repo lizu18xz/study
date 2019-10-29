35909478bb635ac9efd4c82802e2968541d21635
lizu <535733495@qq.com>
2019-10-29 21:48:25 +0800

    读取excel

---
+++ src/main/java/com/fayayo/study/excel/stream/ReaderExcel.java
@@ -0,0 +1,43 @@
+package com.fayayo.study.excel.stream;
+
+import com.monitorjbl.xlsx.StreamingReader;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.*;
+
+/**
+ * @author dalizu on 2019/8/20.
+ * @version v1.0
+ * @desc
+ */
+public class ReaderExcel {
+
+    public static void main(String[] args) {
+
+        try (
+                InputStream is = new FileInputStream(new File("F:/Excel/sss.xlsx"));
+                Workbook workbook = StreamingReader.builder()
+                        .rowCacheSize(100)
+                        .bufferSize(4096)
+                        .open(is)) {
+            for (Sheet sheet : workbook){
+                System.out.println(sheet.getSheetName());
+                for (Row r : sheet) {
+                    for (Cell c : r) {
+                        System.out.println(c.getStringCellValue());
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
